b22c57dc204cab7b5fbd7e6e40c1d7bdae88ebb4
no-more-human <1307951782@qq.com>
2026-08-07 19:34:26 +0800

    feat: 新增 fall_recovery 跌倒检测与自动恢复卡片

--- unitree/g1/main.py
+++ unitree/g1/main.py
@@ -95,6 +95,15 @@ class G1DeviceBundle:
             self._plugins.append(LocoPlugin(plugins_cfg["loco"], namespace, executor, loco_client, slam_client=slam_client, smart_motion=smart_motion))
             print("[bundle] LocoStatePlugin + LocoPlugin loaded")
 
+        if plugins_cfg.get("fall_recovery", {}).get("enabled", False):
+            from device import FallRecoveryPlugin
+            loco_plugin = next((p for p in self._plugins if getattr(p, 'PREFIX', '') == 'loco'), None)
+            self._plugins.append(FallRecoveryPlugin(
+                plugins_cfg["fall_recovery"], namespace, executor,
+                loco_plugin=loco_plugin,
+            ))
+            print("[bundle] FallRecoveryPlugin loaded")
+
         if plugins_cfg.get("continuous_gait", {}).get("enabled", False):
             from device import ContinuousGaitPlugin
             self._plugins.append(ContinuousGaitPlugin(plugins_cfg["continuous_gait"], namespace, executor, loco_client))
